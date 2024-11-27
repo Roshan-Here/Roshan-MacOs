b376040bfef77920710875ca9d8cae172ad1c21f
Muh Roshan <leoparmigiana@gmail.com>
2024-11-27 23:53:08 +0530

    Navbar settign

--- macos-porfolio/src/App.tsx
+++ macos-porfolio/src/App.tsx
@@ -2,21 +2,22 @@ import React, { Dispatch, SetStateAction, useState } from 'react'
 import Navbar from './Components/Navbar';
 
 
-export const [DarkTheme, SetDarkTheme] = useState<boolean>(false)
-
-const handleTheme = (
-  DarkTheme : boolean,
-  SetDarkTheme : Dispatch<SetStateAction<boolean>>
-):void =>{
-  // pass this on NavBar
-  SetDarkTheme(!DarkTheme)
-}
 
 function App() {
+  const [DarkTheme, SetDarkTheme] = useState<boolean>(false)
+
+  const handleTheme = (
+    DarkTheme : boolean,
+    SetDarkTheme : Dispatch<SetStateAction<boolean>>
+  ):void =>{
+    // pass this on NavBar
+    SetDarkTheme(!DarkTheme)
+  }
+  
   return (
     <>
       {/*main div with min-h-screen and width full  */}
-      <div className='min-h-screen  w-full'>
+      <div className='min-h-screen  w-full bg-gray-400'>
         <Navbar DarkTheme={DarkTheme} handleTheme={handleTheme}/>
         {/* fixed top Navbar */}
         {/* CENTER FOR DISPLAYING  */}
--- macos-porfolio/src/Components/Navbar.tsx
+++ macos-porfolio/src/Components/Navbar.tsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { DarkTheme } from "../App";
 import { NavbarProps } from "./Types/NavBarType";
 
-
-function Navbar({DarkTheme,handleTheme}:NavbarProps) {
+function Navbar({ DarkTheme, handleTheme }: NavbarProps) {
   const [currentTheme, SetcurrentTheme] = useState<boolean>(false);
 
   useEffect(() => {
@@ -11,8 +9,17 @@ function Navbar({DarkTheme,handleTheme}:NavbarProps) {
   }, [DarkTheme]);
 
   return (
-    <div className="flex flex-row w-full p-6 bg-transparent opacity-30">
-      {/* Apple icon */}
+    <div className="flex flex-row w-full p-1  bg-opacity-20 border-b border-black border-opacity-20">
+      {/* Apple icon  with on click dropdown*/}
+      <p className="">Apple Icon</p>
+      {/* Open Folders Name */}
+      <p>Folder Name</p>
+
+      {/* end */}
+      <p>Battery icon</p>
+      <p>Wifi Icon</p>
+      <p>More Function Button</p>
+      <p>Date time</p>
     </div>
   );
 }
